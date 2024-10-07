5d0dc7a06bc026a39cee311f4dfb73a988b9a756
mohamed-jilani <mohamedjilani551@gmail.com>
2024-10-08 00:28:07 +0100

    add files

---
+++ src/components/AppHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AppHeader() {
+  return (
+    <header>
+      <h1>My CRUD App</h1>
+      {/* Navigation links or buttons */}
+    </header>
+  );
+}
+
+export default AppHeader;
